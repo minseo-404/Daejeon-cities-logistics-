7a0bbd56b452a5237990e925d2c24a0ef599fb34
minseo-404 <rawfish04@gmail.com>
2026-07-11 18:02:07 +0900

    fix: lean data and reupload

--- data_upload.py
+++ data_upload.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-df= pd.read_csv('data_logistics_total.csv')
+df= pd.read_csv('data_logistics_cleaned.csv')
 
 engine = create_engine('mysql+pymysql://root:xxxx@localhost:3306/mysql', echo=False)
-df.to_sql(name='data_logistics_total', con=engine, if_exists='replace', index=False)
+df.to_sql(name='data_logistics_cleaned', con=engine, if_exists='replace', index=False)
