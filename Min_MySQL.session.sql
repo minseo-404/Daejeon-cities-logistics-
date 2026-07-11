e02d25d1fdfcdfcb33f8cfe3c669b760ee8dfcd9
minseo-404 <rawfish04@gmail.com>
2026-07-11 18:18:19 +0900

    feat: export yearly_total_amount

--- Min_MySQL.session.sql
+++ Min_MySQL.session.sql
@@ -1,5 +1,5 @@
 select 연도, 
     SUM(CASE WHEN 구분 = '출발' THEN 물동량 ELSE 0 END) AS `총 출발량`, 
     SUM(CASE WHEN 구분 = '도착' THEN 물동량 ELSE 0 END) AS `총 도착량`
-from `data_logistics_total`
+from `data_logistics_cleaned`
 group by 연도;
