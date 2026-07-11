34e98f5837082b6f5e1db4be74c64d01a28b02db
minseo-404 <rawfish04@gmail.com>
2026-07-11 17:28:34 +0900

    add SQL query for logistics total

--- Min_MySQL.session.sql
+++ Min_MySQL.session.sql
@@ -1,2 +1,5 @@
-select 연도
-from data_logistics_total.csv
+select 연도, 
+    SUM(CASE WHEN 구분 = '출발' THEN 물동량 ELSE 0 END) AS `총 출발량`, 
+    SUM(CASE WHEN 구분 = '도착' THEN 물동량 ELSE 0 END) AS `총 도착량`
+from `data_logistics_total`
+group by 연도;
